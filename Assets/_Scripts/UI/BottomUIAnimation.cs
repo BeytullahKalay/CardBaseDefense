6a70beaa49980e53376be237debd9675dcd95196
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-03-31 02:39:10 +0300

    add: bottom panel UI animation

--- Assets/_Scripts/UI/BottomUIAnimation.cs
+++ Assets/_Scripts/UI/BottomUIAnimation.cs
@@ -1,16 +1,46 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class BottomUIAnimation : MonoBehaviour
+public class BottomUIAnimation : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float duration;
+    [SerializeField] private float moveDownAmount = 100;
+    
+    private Tween _activeTween;
+    private RectTransform _rectTransform;
+    private Vector3 _pos;
+
+    private void Awake()
     {
-        
+        _rectTransform = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _pos = _rectTransform.anchoredPosition;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MoveUp();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        MoveDown();
+    }
+
+    private void MoveUp()
+    {
+        _activeTween?.Kill();
+        _rectTransform.DOMoveY(_pos.y, duration).SetSpeedBased().SetUpdate(UpdateType.Fixed,false);
+    }
+    
+    private void MoveDown()
+    {
+        _activeTween?.Kill();
+        _rectTransform.DOMoveY(_pos.y - moveDownAmount, duration).SetSpeedBased().SetUpdate(UpdateType.Fixed,false);
     }
 }
