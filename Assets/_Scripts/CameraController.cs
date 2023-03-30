8f640cfb59c3ef165523ee8ded5ec39a99aa7913
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-03-31 02:51:48 +0300

    add: camera controller

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speedMultiplier;
+
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private void FixedUpdate()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+        var inputs = new Vector2(_horizontalInput, _verticalInput);
+        transform.position += (Vector3)inputs * (Time.fixedTime * speedMultiplier);
+    }
+}
