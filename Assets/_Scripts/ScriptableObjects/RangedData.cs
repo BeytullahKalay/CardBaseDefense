aa8b75dfeab715fb4aa0ddab1ce483c33f611e9f
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-04-05 15:59:04 +0300

    modified: fixed damage to random between 2 constant damage

--- Assets/_Scripts/ScriptableObjects/RangedData.cs
+++ Assets/_Scripts/ScriptableObjects/RangedData.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Tower Data")]
 public class RangedData : ScriptableObject
 {
-    [SerializeField] private int damage = 10;
+    [SerializeField] private Vector2Int damage = new Vector2Int(8,12);
     [SerializeField] private float detectEnemyRadius = 5f;
     [SerializeField] private float firingFrequency = 1;
     [SerializeField] private LayerMask whatIsEnemyLayer;
     [SerializeField] private GameObject firePrefab;
 
-    public int Damage => damage;
+    public int Damage => Random.Range(damage.x,damage.y);
 
     public float DetectEnemyRadius => detectEnemyRadius;
     public float FiringFrequency => firingFrequency;
