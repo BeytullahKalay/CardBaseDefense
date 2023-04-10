e4de4f71d8b0d9638d919491e051ff72207f626b
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-04-11 00:42:12 +0300

    modified: Test.cs

--- Assets/_Scripts/Test.cs
+++ Assets/_Scripts/Test.cs
@@ -1,45 +1,115 @@
+using NavMeshPlus.Components;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Test : MonoBehaviour
 {
     [SerializeField] private Tilemap groundTilemap;
+    [SerializeField] private Tilemap underGroundTilemap;
     [SerializeField] private TileBase tile;
+    [SerializeField] private int sizeOfGround = 3;
+    [SerializeField] private NavMeshSurface surface;
+
+    private Vector3Int _lastPos;
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        var mousePosVec2 = Helpers.GetWorldPositionOfPointer(Helpers.MainCamera);
+        var tileAnchor = groundTilemap.tileAnchor;
+        var mousePosVector3 =
+            new Vector3(mousePosVec2.x, mousePosVec2.y, 0) - new Vector3(tileAnchor.x, tileAnchor.y, 0);
+        var vector3IntPos = Vector3Int.RoundToInt(mousePosVector3);
+        var downIntVal = Mathf.FloorToInt(sizeOfGround * .5f);
+        var upIntVal = Mathf.CeilToInt(sizeOfGround * .5f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            var mousePosVec2 = Helpers.GetWorldPositionOfPointer(Helpers.MainCamera);
-            var tileAnchor = groundTilemap.tileAnchor;
+            print(groundTilemap.cellBounds);
+            print(groundTilemap.size);
+        }
 
-            var mousePosVector3 = new Vector3(mousePosVec2.x, mousePosVec2.y, 0) - new Vector3(tileAnchor.x, tileAnchor.y, 0);
+        if (_lastPos != vector3IntPos)
+        {
+            underGroundTilemap.CompressBounds();
+            groundTilemap.CompressBounds();
+
+            CheckBoundaries(vector3IntPos, downIntVal, upIntVal);
+
+
+            underGroundTilemap.ClearAllEditorPreviewTiles();
+            _lastPos = vector3IntPos;
+            underGroundTilemap.EditorPreviewBoxFill(vector3IntPos, tile, vector3IntPos.x - downIntVal,
+                vector3IntPos.y - downIntVal, vector3IntPos.x + upIntVal, vector3IntPos.y + upIntVal);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            underGroundTilemap.BoxFill(vector3IntPos, tile, vector3IntPos.x - downIntVal,
+                vector3IntPos.y - downIntVal, vector3IntPos.x + upIntVal, vector3IntPos.y + upIntVal);
+
+            groundTilemap.BoxFill(vector3IntPos, tile, vector3IntPos.x - downIntVal,
+                vector3IntPos.y - downIntVal, vector3IntPos.x + upIntVal, vector3IntPos.y + upIntVal);
             
-            
-            var vector3IntPos = Vector3Int.RoundToInt(mousePosVector3);
-
-            Vector3Int[] settingTilePositions =
-            {
-                vector3IntPos + new Vector3Int(-1,1,0), // up left
-                vector3IntPos + new Vector3Int(0,1,0), // up
-                vector3IntPos + new Vector3Int(1,1,0), // up right
-                
-                vector3IntPos + new Vector3Int(-1,0,0), // mid left
-                vector3IntPos, // mid
-                vector3IntPos + new Vector3Int(1,0,0), // mid right
-                
-                vector3IntPos + new Vector3Int(-1,-1,0), // down left
-                vector3IntPos + new Vector3Int(0,-1,0), // down
-                vector3IntPos + new Vector3Int(1,-1,0), // down right
-
-            };
-
-            foreach (var t in settingTilePositions)
-            {
-                groundTilemap.SetTile(t,tile);
-            }
-            
+            //surface.bu
+            surface.UpdateNavMesh(surface.navMeshData);
         }
     }
-    
+
+    private void CheckBoundaries(Vector3Int vector3IntPos, int downIntVal, int upIntVal)
+    {
+        if (vector3IntPos.x - downIntVal < underGroundTilemap.origin.x) // left boundaries
+        {
+            var newSize = underGroundTilemap.size;
+            var newOrigin = underGroundTilemap.origin;
+
+            var offset = Mathf.Abs(vector3IntPos.x - downIntVal - underGroundTilemap.origin.x);
+
+            newSize.x += offset;
+            underGroundTilemap.size = newSize;
+            groundTilemap.size = newSize;
+
+            newOrigin.x -= offset;
+            underGroundTilemap.origin = newOrigin;
+            groundTilemap.origin = newOrigin;
+        }
+
+        if (vector3IntPos.x + upIntVal > underGroundTilemap.size.x - Mathf.Abs(underGroundTilemap.origin.x - 1)) // right boundaries
+        {
+            var newSize = underGroundTilemap.size;
+
+            var offset = Mathf.Abs(vector3IntPos.x + upIntVal -
+                                   (underGroundTilemap.size.x - 1 - Mathf.Abs(underGroundTilemap.origin.x)));
+            
+            newSize.x += offset;
+            underGroundTilemap.size = newSize;
+            groundTilemap.size = newSize;
+        }
+
+        if (vector3IntPos.y - downIntVal < underGroundTilemap.origin.y)
+        {
+            var newSize = underGroundTilemap.size;
+            var newOrigin = underGroundTilemap.origin;
+            
+            var offset = Mathf.Abs(vector3IntPos.y - downIntVal - underGroundTilemap.origin.y);
+            
+            newSize.y += offset;
+            underGroundTilemap.size = newSize;
+            groundTilemap.size = newSize;
+
+            newOrigin.y -= offset;
+            underGroundTilemap.origin = newOrigin;
+            groundTilemap.origin = newOrigin;
+        }
+
+        if (vector3IntPos.y + upIntVal > underGroundTilemap.size.y - Mathf.Abs(underGroundTilemap.origin.y - 1)) // upper boundaries
+        {
+            var newSize = underGroundTilemap.size;
+
+            var offset = Mathf.Abs(vector3IntPos.y + upIntVal - (underGroundTilemap.size.y - 1 - Mathf.Abs(underGroundTilemap.origin.y)));
+            
+            newSize.y += offset;
+            underGroundTilemap.size = newSize;
+            groundTilemap.size = newSize;
+        }
+    }
 }
