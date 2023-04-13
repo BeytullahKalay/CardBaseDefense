adeece5f9cc785eff76e720d0e320f57eafcf189
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-04-14 01:53:48 +0300

    add: SetCardsPosition and CallTheWave event to EventManager.cs

--- Assets/_Scripts/Managers/EventManager.cs
+++ Assets/_Scripts/Managers/EventManager.cs
@@ -4,4 +4,6 @@ public static class EventManager
 {
     public static Action UpdateGoldUI;
     public static Action<int> AddThatToCurrentGold;
+    public static Action SetCardsPosition;
+    public static Action CallTheWave;
 }
