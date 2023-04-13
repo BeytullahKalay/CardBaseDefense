319c42e6739281b8124a223b99cb6b704f546eeb
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-04-14 01:51:26 +0300

    add: card positioning like flat

---
+++ Assets/_Scripts/CardPositioner.cs
@@ -0,0 +1,71 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class CardPositioner : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    private float fanAngle;
+    [SerializeField] private float upperAmount;
+    [SerializeField] private float duration = 2f;
+
+    void Start()
+    {
+        SetCardsPosition();
+    }
+
+    private void OnEnable()
+    {
+        EventManager.SetCardsPosition += SetCardsPosition;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.SetCardsPosition -= SetCardsPosition;
+    }
+
+    private void SetCardsPosition()
+    {
+        var allChildren = transform.GetAllChildren();
+        
+        float cardAngleStep = 0;
+        float startAngle = 0;
+        
+        fanAngle = allChildren.Count * 2;
+
+        if (allChildren.Count > 1)
+        {
+            cardAngleStep =fanAngle / (allChildren.Count - 1);
+            startAngle = -(fanAngle / 2f);
+        }
+        else
+        {
+            cardAngleStep = 0;
+            startAngle = 0;
+        }
+
+        float sum = 0;
+
+        for (int i = 0; i < allChildren.Count; i++)
+        {
+            
+            var cardAngle = startAngle + i * cardAngleStep;
+            var x = Mathf.Sin(cardAngle * Mathf.Deg2Rad) * radius;
+            var y = Mathf.Cos(cardAngle * Mathf.Deg2Rad) * radius;
+            sum += y;
+            
+            allChildren[i].GetComponent<RectTransform>().DOLocalMove(new Vector2(x, y), duration);
+            allChildren[i].GetComponent<RectTransform>().DORotate(new Vector3(0f, 0f, -cardAngle),duration);
+        }
+
+        var average = sum / allChildren.Count;
+        transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -average + upperAmount, 0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetCardsPosition();
+        }
+    }
+}
--- Assets/_Scripts/Test.cs
+++ Assets/_Scripts/Test.cs
@@ -1,28 +1,5 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    public int numCards;
-    public float radius;
-    public Image cardPrefab;
-    public float fanAngle;
-    public Canvas canvas;
-
-    void Start()
-    {
-        float cardAngleStep = fanAngle / (numCards - 1);
-        float startAngle = -(fanAngle / 2f);
-
-        for (int i = 0; i < numCards; i++)
-        {
-            float cardAngle = startAngle + i * cardAngleStep;
-            float x = Mathf.Sin(cardAngle * Mathf.Deg2Rad) * radius;
-            float y = Mathf.Cos(cardAngle * Mathf.Deg2Rad) * radius;
-
-            Image card = Instantiate(cardPrefab, canvas.transform);
-            card.rectTransform.anchoredPosition = new Vector2(x, y);
-            card.rectTransform.rotation = Quaternion.Euler(0f, 0f, -cardAngle);
-        }
-    }
 }
