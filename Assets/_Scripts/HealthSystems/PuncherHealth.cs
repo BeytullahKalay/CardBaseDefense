1fd562dbd1aba016a84769b9e102fa1280a20db9
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-04-24 12:34:03 +0300

    add: puncher health basics

---
+++ Assets/_Scripts/HealthSystems/PuncherHealth.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(PuncherStateManager))]
+
+public class PuncherHealth : HealthSystem
+{
+    [SerializeField] private GameObject canvasGameObject;
+
+    private PuncherStateManager _puncherStateManager;
+    
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        _puncherStateManager = GetComponent<PuncherStateManager>();
+    }
+    
+    
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        OnDead += DeadActions;
+    }
+
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        OnDead -= DeadActions;
+    }
+    
+    private void DeadActions()
+    {
+        canvasGameObject.SetActive(false);
+        _puncherStateManager.enabled = false;
+        print("puncher dead");
+        Destroy(gameObject);
+    }
+}
