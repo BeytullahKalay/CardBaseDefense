72ac2abc7a5744d5786bc36205093a056139d0a2
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-04-24 12:39:06 +0300

    created: barrack.cs but doing nothing :)

---
+++ Assets/_Scripts/Card/Barracks.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Barracks : MonoBehaviour
+{
+    [SerializeField] private int solider = 3;
+    
+    [SerializeField] private float spawnRadius = 2f;
+
+    [SerializeField] private GameObject soliderPrefab;
+    
+    
+}
