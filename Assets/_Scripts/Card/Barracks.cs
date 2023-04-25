c65dc7194369e24e68c48383ca70f1d644dc281e
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-04-25 17:19:54 +0300

    add: barrack spawn mechanic

--- Assets/_Scripts/Card/Barracks.cs
+++ Assets/_Scripts/Card/Barracks.cs
@@ -1,12 +1,60 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Barracks : MonoBehaviour
+public class Barracks : ActionCard
 {
-    [SerializeField] private int solider = 3;
+    [SerializeField] private int maxSoliderAmount = 3;
     
-    [SerializeField] private float spawnRadius = 2f;
+    [SerializeField] private float pathRadius = 3f;
+    [SerializeField] private float timeBetweenPaths = 10f;
 
     [SerializeField] private GameObject soliderPrefab;
-    
-    
+
+    [SerializeField] private List<GameObject> spawnedSolider = new List<GameObject>();
+
+    private void OnEnable()
+    {
+        EventManager.WaveCompleted += TrySpawnASolider;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.WaveCompleted -= TrySpawnASolider;
+    }
+
+    private void Start()
+    {
+        TrySpawnASolider(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TrySpawnASolider(true);
+        }
+    }
+
+    private void TrySpawnASolider(bool isWaveCompleted)
+    {
+        print("1");
+        
+        
+        if(!isWaveCompleted) return;
+        print("2");
+
+        if (spawnedSolider.Count < maxSoliderAmount)
+        {
+            print("3");
+            var obj = Instantiate(soliderPrefab);
+            obj.GetComponent<DoRandomPath>().Initialize(transform,pathRadius,timeBetweenPaths,transform.position);
+            obj.GetComponent<HealthSystem>().OnDead += RemoveObject;
+            spawnedSolider.Add(obj); 
+        }
+    }
+
+    private void RemoveObject()
+    {
+        
+    }
 }
