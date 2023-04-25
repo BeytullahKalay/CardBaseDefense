9510ae1d77b11721aff9b80d343d3b459736656b
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-04-25 17:20:29 +0300

    add: new do random path

---
+++ Assets/_Scripts/DoRandomPath.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(PuncherStateManager))]
+public class DoRandomPath : MonoBehaviour
+{
+    private Transform _pathCenterTransform;
+    
+    private float _moveRadius = 3f;
+    private float _timeBetweenPaths = 10f;
+    private float _nextPathTime = float.MinValue;
+
+    private PuncherStateManager _puncherStateManager;
+    
+
+    private void Awake()
+    {
+        _puncherStateManager = GetComponent<PuncherStateManager>();
+    }
+
+    public void Initialize(Transform center, float moveRadius, float timeBetweenPaths,Vector3 spawnPosition)
+    {
+        _pathCenterTransform = center;
+        _moveRadius = moveRadius;
+        _timeBetweenPaths = timeBetweenPaths;
+        transform.position = spawnPosition;
+        SetPathPosition();
+    }
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+    }
+
+    private void Update()
+    {
+        if (Time.time > _nextPathTime && _puncherStateManager.UnitStates != UnitStates.Attack)
+        {
+            SetPathPosition();
+        }
+    }
+
+    private void SetPathPosition()
+    {
+        _puncherStateManager.MovePos = (Vector2)_pathCenterTransform.position + Random.insideUnitCircle * _moveRadius;
+        _nextPathTime = Time.time + _timeBetweenPaths;
+    }
+}
