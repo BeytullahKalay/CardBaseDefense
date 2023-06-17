ceefe494062ad480ebd156b3dbcb82146d0a76c4
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-06-17 11:45:44 +0300

    add: mouse state manager

---
+++ Assets/_Scripts/Managers/MouseStateManager.cs
@@ -0,0 +1,22 @@
+
+public class MouseStateManager : MonoSingleton<MouseStateManager>
+{
+    private MouseState _mouseState;
+
+    public MouseState MouseState => _mouseState;
+
+    private void OnEnable()
+    {
+        EventManager.SetMouseStateTo += SetMouseStateTo;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.SetMouseStateTo += SetMouseStateTo;
+    }
+    
+    private void SetMouseStateTo(MouseState mouseState)
+    {
+        _mouseState = mouseState;
+    }
+}
